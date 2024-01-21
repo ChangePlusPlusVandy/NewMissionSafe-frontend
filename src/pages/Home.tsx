c7fef387fb55d87a601955e9ad92bd61e16e8a1d
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-01-21 00:01:43 -0600

    implements youth part (completely untested)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 import { getEventsByDate } from "../utils/eventInterface";
-import { getActiveYouth } from "../utils/youthInterface";
+import { getYouthByProgram } from "../utils/youthInterface";
+import { getStaffByID } from "../utils/staffInterface";
 import { returnedEventType } from "../utils/models/eventModel";
+import { staffType } from "../utils/models/staffModel";
 import Event from "../components/Event";
 import { youthType } from "../utils/models/youthModel";
 import "./Home.css";
@@ -17,25 +19,40 @@ import "./Home.css";
 
 const Home: React.FC = () => {
   const { currentUser } = useAuth();
-  const [events, setEvents] = useState<returnedEventType[]>([]);
-  const [youth, setYouth] = useState<youthType[]>([]);
-  const [eventsError, setEventsError] = useState<string>("");
-  const [youthError, setYouthError] = useState<string>("");
-  const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState<{
+    token: string;
+    userId: string;
+  }>({ token: "", userId: "" });
 
   useEffect(() => {
-    currentUser?.getIdToken().then((token) => {
-      if (token == null) {
+    currentUser?.getIdToken().then((t) => {
+      if (t == null) {
         //review: is this a good way to handle this?
         //probably use this to determine loading state
         console.log("No token available");
         //navigate("/login");
       } else {
-        void getTodayEvents(token);
-        void getYouth(token);
+        setUserDetails({
+          token: t,
+          userId: currentUser.uid,
+        });
       }
     });
+  }, [currentUser]);
 
+  return (
+    <>
+      <TodayEvents token={userDetails.token}></TodayEvents>
+      <ProgramYouth token={userDetails.token} userId={userDetails.userId}></ProgramYouth>
+    </>
+  );
+};
+
+const TodayEvents: React.FC<{ token: string }> = ({ token }) => {
+  const [events, setEvents] = useState<returnedEventType[]>([]);
+  const [eventsError, setEventsError] = useState<string>("");
+
+  useEffect(() => {
     const getTodayEvents = async (token: string) => {
       try {
         const currentDate = new Date();
@@ -43,49 +60,137 @@ const Home: React.FC = () => {
         const todayEvents = await getEventsByDate(token, currentDate);
         setEvents(todayEvents);
       } catch (err) {
-        //todo: error handling
         setEventsError("Failed to retrieve today's events");
         console.log(err);
       }
     };
-    const getYouth = async (token: string) => {
+
+    if (token) {
+      void getTodayEvents(token);
+    }
+  }, [token]);
+
+  return (
+    <section>
+      <h1>Today's Events</h1>
+      {(() => {
+        if (eventsError != "") {
+          return <h2 className="error">{eventsError}</h2>;
+        } else if (events.length == 0) {
+          return <h2>No events today</h2>;
+        } else {
+          return (
+            <ul>
+              {events.map((i) => (
+                <li>
+                  <Event
+                    eventName={i.name}
+                    eventDate={i.date}
+                    key={i.code}
+                  ></Event>
+                </li>
+              ))}
+            </ul>
+          );
+        }
+      })()}
+    </section>
+  );
+};
+
+const ProgramYouth: React.FC<{ token: string; userId: string }> = ({
+  token,
+  userId,
+}) => {
+  const [youth, setYouth] = useState<youthType[]>([]);
+  const [youthError, setYouthError] = useState<string>("");
+  const [programName, setProgramName] = useState<string>("");
+
+  useEffect(() => {
+    const getYouth = async (t: string, uid: string) => {
       try {
-        //todo: right now this is getting all active youth, pretty sure it's supposed to be getting a subset but not sure what
-        const youth = await getActiveYouth(token);
-        console.log("gotten youth: ", youth);
-        setYouth(youth);
+        //review: we need to make sure all staff who get created are associated with 1 program (types, input form, etc)
+        const currentStaffMember: staffType = await getStaffByID(uid, t);
+        if (currentStaffMember.programs.length == 0) {
+          setYouthError("You are not associated with a program");
+        } else {
+          setProgramName(currentStaffMember.programs[0]);
+          const youth = await getYouthByProgram(
+            currentStaffMember.programs[0],
+            t
+          );
+          setYouth(youth);
+        }
       } catch (err) {
-        //todo: error handling
         setYouthError("Failed to retrieve youth");
         console.log(err);
       }
     };
-  }, [currentUser]);
+
+    if (token && userId) {
+      void getYouth(token, userId);
+    }
+  }, [token]);
 
   return (
-    <>
-      <section>
-        <h1>Today's Events</h1>
-        {events.length == 0 ? (
-          //todo: styling
-          <h2>No events today</h2>
-        ) : (
-          <ul>
-            {events.map((i) => (
-              <li>
-                <Event
-                  eventName={i.name}
-                  eventDate={i.date}
-                  key={i.code}
-                ></Event>
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
-      <section></section>
-    </>
+    <section>
+      {programName == "" ? (
+        <h1>Youth In Your Program</h1>
+      ) : (
+        <h1>Youth In {programName}</h1>
+      )}
+
+      {(() => {
+        if (youthError != "") {
+          return <h2 className="error">{youthError}</h2>;
+        } else if (youth.length == 0) {
+          return <h2>No youth found</h2>;
+        } else {
+          return (
+            <ul>
+              {youth.map((i) => (
+                <li>
+                  <FillerYouth
+                    name={i.firebaseUID + i.lastName}
+                    key={i.firebaseUID}
+                  ></FillerYouth>
+                </li>
+              ))}
+            </ul>
+          );
+        }
+      })()}
+    </section>
   );
+};
+
+const FillerYouth: React.FC<{ name: string}> = ({name}) => {
+	const elementStyle = {
+		paddingTop: '20px',
+		paddingBottom: '12px',
+		width: '250px',
+		height: '100%',
+		boxSizing: 'border-box' as 'border-box',
+	
+		display: 'flex',
+		flexDirection: 'row' as 'row',
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+	
+		background: '#FFF',
+		
+		position: 'relative' as 'relative',
+		overflow: 'hidden',
+	
+		borderBottom: '1px solid #C0C0C0',
+		margin: '5px 25px'
+	};
+
+	return (
+		<div style={elementStyle}>
+			{name}
+		</div>
+	)
 };
 
 export default Home;
---
+++ src/utils/models/staffModel.tsx
@@ -0,0 +1,10 @@
+export interface staffType {
+	firstName: string;
+	lastName: string;
+	email: string;
+	firebaseUID: string;
+	programs: string[];
+	active: boolean;
+	counselor: boolean;
+	admin: boolean;
+}
