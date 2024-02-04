9530071bb8024e10175c75533e55ae42db34ff66
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-02-04 12:26:12 -0600

    adds figma styles to mantine theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,35 @@ import "./index.css";
 import { MantineProvider, createTheme } from "@mantine/core";
 import "@mantine/core/styles.css";
 
-const theme = createTheme({});
+const theme = createTheme({
+  colors: {
+    missionSafeBlue: [
+      "#ebf8fe",
+      "#d7edf9",
+      "#aadaf5",
+      "#7bc7f1",
+      "#59b6ef",
+      "#47abee",
+      "#3da6ef",
+      "#3291d4",
+      "#2581be",
+      "#022B41",
+    ],
+  },
+  fontFamily: "Almarai, sans-serif",
+  headings: {
+    fontFamily: "Arimo, sans-serif",
+    fontWeight: "700",
+    sizes: {
+      h1: {
+        fontSize: "25px",
+      },
+      h2: {
+        fontSize: "22px",
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MantineProvider theme={theme}>
