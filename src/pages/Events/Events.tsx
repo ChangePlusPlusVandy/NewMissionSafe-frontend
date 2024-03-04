f6b773fbd2b5b7e5b57e1556700f1808e4bf79ed
Bobby Mi11er <bobby@tybeemillers.com>
2024-03-04 15:24:04 -0600

    Update events page

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -3,10 +3,21 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../AuthContext";
 import { getAllEvents } from "../../utils/eventInterface";
 import { eventType } from "../../utils/models/eventModel";
-import "./Events.css";
 import Event from "../../components/Event";
-import { Paper, Button, Autocomplete} from "@mantine/core";
-import { IconSearch } from '@tabler/icons-react';
+import {IconAdjustmentsHorizontal} from "@tabler/icons-react";
+
+import {
+  Group,
+  Button,
+  TextInput,
+  ScrollArea,
+  Title,
+  Center,
+  Flex,
+  Stack,
+  Space,
+} from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
 
 const Events: React.FC = () => {
   const { currentUser } = useAuth();
@@ -15,7 +26,7 @@ const Events: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [events, setEvents] = useState<[eventType]>();
 
-  const icon = <IconSearch style={{color:"grey"}}/>;
+  const icon = <IconSearch style={{ color: "grey" }} />;
 
   useEffect(() => {
     async function fetchEvents() {
@@ -39,38 +50,102 @@ const Events: React.FC = () => {
   const renderEvents = () => {
     if (events?.length != undefined && events.length > 0) {
       return (
-        <div className="event-card-container">
-          {events?.map((item) => {
-            return <Event eventName={item.name} eventDate={item.date} />;
-          })}
-        </div>
+        <Center>
+          <ScrollArea type="never" h={"40vh"} w={"90%"}>
+            <Flex direction={"column"} gap="md">
+              {events?.map((item) => {
+                return (
+                  <Event
+                    eventName={item.name}
+                    eventDate={item.date}
+                    eventDes={item.description}
+                  />
+                );
+              })}
+            </Flex>
+          </ScrollArea>
+        </Center>
       );
     } else {
       return (
-        <div>
-          <p>There are no events currently</p>
-        </div>
+        <Center>
+          <Title c="white" order={4}>
+            There are no events currently
+          </Title>
+        </Center>
       );
     }
   };
 
   return (
-    <Paper bg={"missionSafeBlue.9"} w={"100%"} h={"100%"} radius={0}>
-      <div className="main-container">
-        <h1>Events</h1>
-        {isLoading ? (
-          <p>Loading Events...</p>
-        ) : (
-          <div className="sub-container">
-            <Button variant="filled" bg={"missionSafeRed.9"} loading={isLoading} onClick={handleCreateEvent}>Create New Event</Button>
-            <Autocomplete bg={"missionSafeBlue.9"} placeholder="Search" leftSection={icon}/>
-            <br />
-            {renderEvents()}
-          </div>
-        )}
-      </div>
-    </Paper>
+    <Stack bg={"missionSafeBlue.9"} h={"100vh"}>
+      <Space h="md" />
+      <Stack style={!isLoading ? { marginBottom: "auto" } : {}}>
+        <Center>
+          <Title order={1} c="white">
+            Events
+          </Title>
+        </Center>
+        <Center>
+          <Button
+            variant="filled"
+            bg={"missionSafeRed.9"}
+            loading={isLoading}
+            onClick={handleCreateEvent}
+            w={"90%"}
+          >
+            Create New Event
+          </Button>
+        </Center>
+      </Stack>
+      {isLoading ? (
+        <Center>
+          <Title c="white" order={4}>
+            Loading Events...
+          </Title>
+        </Center>
+      ) : (
+        <Stack
+          justify="flex-end"
+          align="stretch"
+          style={{ paddingBottom: "10%" }}
+        >
+          <Center>
+            <Title order={2} c="white">
+              Past Events
+            </Title>
+          </Center>
+          <Center>
+          <Group justify="space-between" gap="0" w={"85%"}>
+            <TextInput
+              variant="unstyled"
+              placeholder="Search"
+              w={"90%"}
+              leftSection={icon}
+              style={{
+                boxShadow: "0 0 4px 2px rgba(255,255,255,0.4)",
+                borderRadius: "5px",
+              }}
+              styles={{
+                input: {
+                  color: "white",
+                },
+              }}
+            />
+            <IconAdjustmentsHorizontal width="10%" color="white" stroke={1.2}/>
+          </Group>
+          </Center>
+          
+          <Space />
+          {renderEvents()}
+        </Stack>
+      )}
+    </Stack>
   );
 };
 
 export default Events;
+
+/**
+ *
+ */
